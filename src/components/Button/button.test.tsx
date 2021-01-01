9fef55fc9e1a09a90e5c3345d86bf26087ecc743
Ori Afias <afiasori@gmail.com>
2021-01-01 21:54:44 +0200

    testing workfloww

--- src/components/Button/button.test.tsx
+++ src/components/Button/button.test.tsx
@@ -4,10 +4,10 @@ import { fireEvent, render, cleanup } from '@testing-library/react';
 import { Button, ButtonProps } from './Button';
 
 describe('render', () => {
-	let props: ButtonProps = { label: 'this is a button' };
+	let props: ButtonProps = { label: 'this is button' };
 
 	it('renders the button', () => {
 		const { queryByText } = render(<Button {...props} />);
-		expect(queryByText('this is a button'));
+		expect(queryByText('this is button'));
 	});
 });
